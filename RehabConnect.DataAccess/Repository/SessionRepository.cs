e2b74c90d092a8dff1566517900127d5a63051b9
MUHAMMAD FIKRI BIN SHARUNAZIM <93756712+fkrshrnzm@users.noreply.github.com>
2024-06-25 02:48:26 +0800

    models tak selari dengan migration

---
+++ RehabConnect.DataAccess/Repository/SessionRepository.cs
@@ -0,0 +1,31 @@
+﻿using RehabConnect.DataAccess.Data;
+using RehabConnect.DataAccess.Repository.IRepository;
+using RehabConnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehabConnect.DataAccess.Repository
+{
+    public class SessionRepository : Repository<Session>, ISessionRepository
+    {
+        private ApplicationDbContext _db;
+
+        public SessionRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Schedule obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
